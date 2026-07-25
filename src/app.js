88a905caf3eed8cf84a36ba948f401627256a296
Umashankar S <umashankars111333@gmail.com>
2026-07-25 18:17:17 +0530

    Fixed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const statusCodeExercises = require('./status-code-exercises');
 const app = express();
 
 app.use(express.json());
+// Also expose the exercise routes at their short URLs for classroom testing.
+app.use(statusCodeExercises);
 app.use('/api/status-exercises', statusCodeExercises);
 app.use(routes);
 
